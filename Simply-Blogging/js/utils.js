930b329d65cc3a1bc31b3f2e2e3a9968c591d757
gohurali <gohurali@live.com>
2021-03-16 15:25:57 -0700

    working on sessionStorage for now

--- Simply-Blogging/js/utils.js
+++ Simply-Blogging/js/utils.js
@@ -1,6 +1,7 @@
 var row_count = 0
 var col_count = 0
 current_cells = []
+blogs = []
 
 function getBlogData(){
     var blog_title = document.getElementById("title").value
@@ -11,10 +12,35 @@ function getBlogData(){
     console.log(blog_desc)
     console.log(blog_image)
 
-    postBlog(blog_title,blog_desc,blog_image)
+    // Stores the Data in storage
+    storeData(blog_title,blog_desc,blog_image)
+
+    //Retreive Data from storage
+    var blog_data = getStorageData()
+
+    for(var i = 0; i < blog_data.length; i++){
+        //for(var i = blog_data.length - 1; i >= 0 ; i--){
+        var title = blog_data[i].title
+        var desc = blog_data[i].desc
+        var img = blog_data[i].img
+        console.log("------ " + title + " " + desc + " " + img +  " ------")
+        postBlog(blog_title,blog_desc,blog_image)
+    }
+
+    
     
     // Once submitted Clear the form
     clearBlogForm()
+}
+
+function create_row(){
+    var table = document.getElementById('blogPostTable')
+    var tbody = document.getElementsByTagName('tbody')[0]
+    var new_row = tbody.insertRow(-1)
+    var cell1 = new_row.insertCell(0)
+    var cell2 = new_row.insertCell(1)
+    var cell3 = new_row.insertCell(2)
+    return [cell1,cell2,cell3]
 }
 
 function postBlog(title,desc,img){
@@ -31,7 +57,7 @@ function postBlog(title,desc,img){
 }
 
 function get_inner_HTML(title,desc,img){
-    return "<div class='addBlog'><h4>" + title +"</h4>" + "<br/>" + desc + "</div>"
+    return "<div class='smallBlog'><h4>" + title +"</h4>" + "<br/>" + desc + "</div>"
 }
 
 
@@ -43,16 +69,18 @@ function clearBlogForm(){
 
 }
 
+// Storage Functions
+function storeData(title,desc,img){
+    blog_data = {}
+    blog_data.title = title
+    blog_data.desc = desc
+    blog_data.img = img
+    blogs.push(blog_data)
+    json_blog_data = JSON.stringify(blogs)
+    sessionStorage.setItem("blogs",json_blog_data)
+}
 
-/////////////////////////////
-// Table utils
-/////////////////////////////
-function create_row(){
-    var table = document.getElementById('blogPostTable')
-    var tbody = document.getElementsByTagName('tbody')[0]
-    var new_row = tbody.insertRow(-1)
-    var cell1 = new_row.insertCell(0)
-    var cell2 = new_row.insertCell(1)
-    var cell3 = new_row.insertCell(2)
-    return [cell1,cell2,cell3]
+function getStorageData(){
+    var blog_data = JSON.parse(sessionStorage.getItem("blogs"))
+    return blog_data
 }
