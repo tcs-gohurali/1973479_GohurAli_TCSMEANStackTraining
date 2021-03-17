34fa018032da227821f087afff0aba0cd3c6f15f
gohurali <gohurali@live.com>
2021-03-17 14:34:34 -0700

    Working with Grid System

--- Simply-Blogging/js/utils.js
+++ Simply-Blogging/js/utils.js
@@ -19,24 +19,66 @@ function getBlogData(){
     console.log(blog_desc)
     console.log(blog_image)
 
-    // Stores the Data in storage
+    // Stores the Data in localStorage
     storeData(blog_title,blog_desc,blog_image)
-
-    postBlog(blog_title,blog_desc,blog_image)
+    //postBlog(blog_title,blog_desc,blog_image)
+    postBlog2(blog_title,blog_desc,blog_image)
     clearBlogForm()
 }
 
-function create_row(){
-    var table = document.getElementById('blogPostTable')
-    var tbody = document.getElementsByTagName('tbody')[0]
-    var new_row = tbody.insertRow(-1)
-    var cell1 = new_row.insertCell(0)
-    var cell2 = new_row.insertCell(1)
-    var cell3 = new_row.insertCell(2)
-    return [cell1,cell2,cell3]
+function postBlog2(title,desc,img){
+    console.log("Made it to postBlog!")
+    if(row_count == 0 && col_count == 0){
+        console.log("nothing yet..")
+        create_grid_row()
+        col_count = 0
+    }
+    else if(col_count == 3){
+        row_count++
+        create_grid_row()
+        col_count = 0
+    }
+    
+    var curr_col = create_grid_col()
+    
+    var inner_html = get_inner_HTML(title,desc,img)
+    curr_col.innerHTML = inner_html
+    col_count++
 }
 
+function create_grid_row(){
+    var gridStart = document.getElementById("blogPostsGrid")
+    var new_row = document.createElement('div')
+    new_row.id = row_count
+    new_row.className = 'row'
+    gridStart.appendChild(new_row)
+}
+
+function create_grid_col(){
+    var gridStart = document.getElementById("blogPostsGrid")
+    console.log(gridStart)
+    console.log("curr_row num = " + row_count)
+    var curr_row = document.getElementById(row_count.toString())
+    console.log(curr_row)
+    var new_col = document.createElement('div')
+    new_col.className = 'col'
+    curr_row.appendChild(new_col)
+    console.log("created col...")
+    return new_col
+}
+
+
 function postBlog(title,desc,img){
+    // nested function which is responsible to inserting a new row of 3 cols
+    function create_row(){
+        var table = document.getElementById('blogPostTable')
+        var tbody = document.getElementsByTagName('tbody')[0]
+        var new_row = tbody.insertRow(-1)
+        var cell1 = new_row.insertCell(0)
+        var cell2 = new_row.insertCell(1)
+        var cell3 = new_row.insertCell(2)
+        return [cell1,cell2,cell3]
+    }
     console.log("Made it to postBlog!")
     if( (row_count == 0 && col_count == 0) || col_count == 3){
         current_cells = create_row()
@@ -51,8 +93,7 @@ function postBlog(title,desc,img){
 
 function retreiveData(){
     //Retreive Data from storage
-    var blog_data = JSON.parse(localStorage.getItem("blogs")) //getStorageData()
-
+    var blog_data = JSON.parse(localStorage.getItem("blogs"))
     //console.log("data from SStorage = ", JSON.stringify(blog_data))
 
     for(var i = 0; i < blog_data.length; i++){
@@ -63,10 +104,24 @@ function retreiveData(){
         console.log("------ " + title + " " + desc + " " + img +  " ------")
         postBlog(title,desc,img)
     }
+}
+
+function retreiveData2(){
+    //Retreive Data from storage
+    var blog_data = JSON.parse(localStorage.getItem("blogs"))
+    //console.log("data from SStorage = ", JSON.stringify(blog_data))
+
+    for(var i = 0, count = 1; i < blog_data.length; i++,count++){
+        //for(var i = blog_data.length - 1; i >= 0 ; i--){
+        var title = blog_data[i].title
+        var desc = blog_data[i].desc
+        var img = blog_data[i].img
+        console.log("------ " + title + " " + desc + " " + img +  " ------")
+        postBlog2(title,desc,img)
+    }
 }    
 
 function get_inner_HTML(title,desc,img){
-    console.log("BROOOOO -> " + img)
     if(!img){
         return `
         <div class='smallBlog'>
@@ -92,7 +147,6 @@ function get_inner_HTML(title,desc,img){
     }
 }
 
-
 function clearBlogForm(){
 
     document.getElementById("title").value = ""
@@ -103,6 +157,9 @@ function clearBlogForm(){
 
 // Storage Functions
 function storeData(title,desc,img){
+    function checkStorageExists(){
+        return localStorage.getItem('blogs') != null
+    }
 
     var storage_exists = checkStorageExists()
 
@@ -125,15 +182,6 @@ function storeData(title,desc,img){
         json_blog_data = JSON.stringify(blogs)
         localStorage.setItem("blogs",json_blog_data)
     }
-}
-
-function checkStorageExists(){
-    return localStorage.getItem('blogs') != null
-}
-
-function getStorageData(){
-    var blog_data = JSON.parse(localStorage.getItem("blogs"))
-    return blog_data
 }
 
 function clearLocalStorage(){
