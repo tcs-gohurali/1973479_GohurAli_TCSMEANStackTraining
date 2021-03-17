d87190a980b9454a3ced100652f63963727efa6b
gohurali <gohurali@live.com>
2021-03-17 15:25:27 -0700

    Reverted back to table -- comments++

--- Simply-Blogging/js/utils.js
+++ Simply-Blogging/js/utils.js
@@ -21,12 +21,117 @@ function getBlogData(){
 
     // Stores the Data in localStorage
     storeData(blog_title,blog_desc,blog_image)
-    //postBlog(blog_title,blog_desc,blog_image)
-    postBlog2(blog_title,blog_desc,blog_image)
+    postBlog(blog_title,blog_desc,blog_image)
     clearBlogForm()
 }
 
-function postBlog2(title,desc,img){
+function postBlog(title,desc,img){
+    // nested function which is responsible to inserting a new row of 3 cols
+    function create_row(){
+        var table = document.getElementById('blogPostTable')
+        var tbody = document.getElementsByTagName('tbody')[0]
+        var new_row = tbody.insertRow(-1)
+        var cell1 = new_row.insertCell(0)
+        var cell2 = new_row.insertCell(1)
+        var cell3 = new_row.insertCell(2)
+        return [cell1,cell2,cell3]
+    }
+    console.log("Made it to postBlog!")
+    if( (row_count == 0 && col_count == 0) || col_count == 3){
+        current_cells = create_row()
+        col_count = 0
+    }
+    var curr_cell = current_cells[col_count]
+
+    var inner_html = get_inner_HTML(title,desc,img)
+    curr_cell.innerHTML = inner_html
+    col_count++
+}
+
+function retreiveData(){
+    //Retreive Data from storage
+    var blog_data = JSON.parse(localStorage.getItem("blogs"))
+    //console.log("data from SStorage = ", JSON.stringify(blog_data))
+
+    for(var i = 0; i < blog_data.length; i++){
+        //for(var i = blog_data.length - 1; i >= 0 ; i--){
+        var title = blog_data[i].title
+        var desc = blog_data[i].desc
+        var img = blog_data[i].img
+        console.log("------ " + title + " " + desc + " " + img +  " ------")
+        postBlog(title,desc,img)
+    }
+}
+
+function get_inner_HTML(title,desc,img){
+    if(!img){
+        return `
+        <div class='smallBlog'>
+            <h4>${title}</h4>
+            <div class="smallBlogDesc">
+                <p>${desc}</p>
+            </div>
+        </div>`
+    }
+    else{
+        return `
+            <div class='smallBlog'>
+                <h4>${title}</h4>
+                <div class="smallBlogDesc">
+                    <p>${desc}</p>
+                </div>
+                <div class="thumbnail">
+                    <a href="${img}">
+                        <image class="smallBlogImg" src="${img}"></image>
+                    </a>
+                </div>
+            </div>`
+    }
+}
+
+function clearBlogForm(){
+    document.getElementById("title").value = ""
+    document.getElementById("desc").value = ""
+    document.getElementById("img").value = ""
+}
+
+// Storage Functions
+function storeData(title,desc,img){
+    function checkStorageExists(){
+        return localStorage.getItem('blogs') != null
+    }
+
+    var storage_exists = checkStorageExists()
+
+    if(storage_exists){
+        // since the storage exists, we need to get it and update it
+        var list_of_blogs = JSON.parse(localStorage.getItem('blogs'))
+        blog_data = {}
+        blog_data.title = title
+        blog_data.desc = desc
+        blog_data.img = img
+        list_of_blogs.push(blog_data)
+        localStorage.setItem("blogs",JSON.stringify(list_of_blogs))   
+    }
+    else{
+        blog_data = {}
+        blog_data.title = title
+        blog_data.desc = desc
+        blog_data.img = img
+        blogs.push(blog_data)
+        json_blog_data = JSON.stringify(blogs)
+        localStorage.setItem("blogs",json_blog_data)
+    }
+}
+
+function clearLocalStorage(){
+    localStorage.clear()
+}
+
+// [Ignore]: Unused functions for grid format of the recently posted blogs
+var table_mode = true
+var grid_mode = false
+function gridPostBlog(title,desc,img){
     console.log("Made it to postBlog!")
     if(row_count == 0 && col_count == 0){
         console.log("nothing yet..")
@@ -67,46 +172,7 @@ function create_grid_col(){
     return new_col
 }
 
-
-function postBlog(title,desc,img){
-    // nested function which is responsible to inserting a new row of 3 cols
-    function create_row(){
-        var table = document.getElementById('blogPostTable')
-        var tbody = document.getElementsByTagName('tbody')[0]
-        var new_row = tbody.insertRow(-1)
-        var cell1 = new_row.insertCell(0)
-        var cell2 = new_row.insertCell(1)
-        var cell3 = new_row.insertCell(2)
-        return [cell1,cell2,cell3]
-    }
-    console.log("Made it to postBlog!")
-    if( (row_count == 0 && col_count == 0) || col_count == 3){
-        current_cells = create_row()
-        col_count = 0
-    }
-    var curr_cell = current_cells[col_count]
-
-    var inner_html = get_inner_HTML(title,desc,img)
-    curr_cell.innerHTML = inner_html
-    col_count++
-}
-
-function retreiveData(){
-    //Retreive Data from storage
-    var blog_data = JSON.parse(localStorage.getItem("blogs"))
-    //console.log("data from SStorage = ", JSON.stringify(blog_data))
-
-    for(var i = 0; i < blog_data.length; i++){
-        //for(var i = blog_data.length - 1; i >= 0 ; i--){
-        var title = blog_data[i].title
-        var desc = blog_data[i].desc
-        var img = blog_data[i].img
-        console.log("------ " + title + " " + desc + " " + img +  " ------")
-        postBlog(title,desc,img)
-    }
-}
-
-function retreiveData2(){
+function grid_retrieve_data(){
     //Retreive Data from storage
     var blog_data = JSON.parse(localStorage.getItem("blogs"))
     //console.log("data from SStorage = ", JSON.stringify(blog_data))
@@ -117,73 +183,33 @@ function retreiveData2(){
         var desc = blog_data[i].desc
         var img = blog_data[i].img
         console.log("------ " + title + " " + desc + " " + img +  " ------")
-        postBlog2(title,desc,img)
+        gridPostBlog(title,desc,img)
     }
-}    
+}  
 
-function get_inner_HTML(title,desc,img){
-    if(!img){
-        return `
-        <div class='smallBlog'>
-            <h4>${title}</h4>
-            <div class="smallBlogDesc">
-                <p>${desc}</p>
-            </div>
-        </div>`
+function show_data(){
+
+    var mode = localStorage.getItem("mode")
+    if(mode == 'table'){
+        retreiveData
     }
-    else{
-        return `
-            <div class='smallBlog'>
-                <h4>${title}</h4>
-                <div class="smallBlogDesc">
-                    <p>${desc}</p>
-                </div>
-                <div class="thumbnail">
-                    <a href="${img}">
-                        <image class="smallBlogImg" src="${img}"></image>
-                    </a>
-                </div>
-            </div>`
+    else if(model == 'grid'){
+        var grid_loc = document.getElementById("gridBlogLocation")
+        var grid = document.createElement('div')
+        grid.id = 'blogPostsGrid'
+        grid_retrieve_data();
     }
 }
 
-function clearBlogForm(){
-
-    document.getElementById("title").value = ""
-    document.getElementById("desc").value = ""
-    document.getElementById("img").value = ""
-
+function table_mode_enable(){
+    grid_mode = false
+    table_mode = true
+    localStorage.setItem('mode','table')
+    return table_mode
 }
-
-// Storage Functions
-function storeData(title,desc,img){
-    function checkStorageExists(){
-        return localStorage.getItem('blogs') != null
-    }
-
-    var storage_exists = checkStorageExists()
-
-    if(storage_exists){
-        // since the storage exists, we need to get it and update it
-        var list_of_blogs = JSON.parse(localStorage.getItem('blogs'))
-        blog_data = {}
-        blog_data.title = title
-        blog_data.desc = desc
-        blog_data.img = img
-        list_of_blogs.push(blog_data)
-        localStorage.setItem("blogs",JSON.stringify(list_of_blogs))   
-    }
-    else{
-        blog_data = {}
-        blog_data.title = title
-        blog_data.desc = desc
-        blog_data.img = img
-        blogs.push(blog_data)
-        json_blog_data = JSON.stringify(blogs)
-        localStorage.setItem("blogs",json_blog_data)
-    }
-}
-
-function clearLocalStorage(){
-    localStorage.clear()
+function grid_mode_enable(){
+    table_mode = false
+    grid_model = true
+    localStorage.setItem('mode','grid')
+    return grid_mode
 }
