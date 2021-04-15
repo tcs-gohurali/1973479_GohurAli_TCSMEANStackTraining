7e09cdfb966bd2a5e4810078b1a1ed281e57523a
gohurali <gohurali@live.com>
2021-04-15 15:15:24 -0700

    [DONE]: mongoose & mongodb version

---
+++ Call-Records-Analysis/mongodb_insert.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const mongo_client = require("mongodb").MongoClient
+
+const config = {
+	URL: "mongodb://localhost:27017",
+	db_name:"calls",
+	table_name:"callRecords"
+}
+
+function open_data(loc){
+	return JSON.parse(fs.readFileSync(loc,'utf-8'))
+}
+
+function emplate_into_DB(data){
+
+	const vars = { useUnifiedTopology:true }
+	mongo_client.connect(config['URL'],vars,(error,client)=>{
+		if(!error){
+			let db = client.db(config['db_name'])
+			db.collection("callRecords").insertMany(data,(error2,result)=>{
+				if(!error2){
+					console.log("[LOG]: Success! Inserted!")
+				}else{
+					console.log('[LOG]: Insertion Error!')
+					console.log(error)
+				}
+				client.close()
+			})
+		}else{
+			console.log('[LOG]: Error!')
+			console.log(error)
+		}
+	})
+}
+
+let data = open_data('call_data.json')
+emplate_into_DB(data)
---
+++ Call-Records-Analysis/mongoose_insert.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+const config = {
+	URL: "mongodb://localhost:27017",
+	db_name:"calls",
+	table_name:"callRecords"
+}
+
+const CallRecordsSchema = mongoose.Schema({
+	_id:Number,
+	source:String,
+	destination:String,
+	sourceLocation:String,
+	destinationLocation:String,
+	callDuration:String,
+	roaming:String,
+	callCharge:String
+})
+
+function open_data(loc){
+	return JSON.parse(fs.readFileSync(loc,'utf-8'))
+}
+
+function emplate_into_DB(data){
+
+	const vars = {
+		useNewUrlParser: true,
+		useUnifiedTopology:true
+	}
+
+	mongoose.connect(config['URL'] + "/" + config['db_name'],vars)
+	const db = mongoose.connection
+
+	db.on("error",(err)=>console.log(err))
+	db.once("open",()=>{
+		const ProductModel = mongoose.model("",CallRecordsSchema,config['table_name'])
+		ProductModel.insertMany(data,(error,result)=>{
+			if(!error){
+				console.log("Inserted!")
+			}
+			else{
+				console.log(error.message)
+			}
+			mongoose.disconnect()
+		})	
+	})
+}
+
+
+let data = open_data('call_data.json')
+emplate_into_DB(data)
